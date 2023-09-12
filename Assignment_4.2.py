57fe8f8d34ca29f2fb7c01a570f5a8bae068adf5
deena777 <142565284+deena777@users.noreply.github.com>
2023-09-12 22:46:01 +0530

    Add files via upload

---
+++ Assignment_4.2.py
@@ -0,0 +1,23 @@
+'''
+# Find the way with Maps
+
+# Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+sample list: [1, 2, 3, 4, 5, 6, 7]
+
+Triple of list numbers: [3, 6, 9, 12, 15, 18, 21]
+'''
+
+
+# input from user
+size = (int(input("Enter the length of the list: ")))
+lst = []
+for i in range (size):
+    ele = int(input("Enter the elements:"))
+    lst.append(ele)
+print("list : ",lst)
+
+# tripling of list
+triple = lambda x : x*3
+triple_of_lst = list(map(triple,lst))
+print("Triple of list numbers : ",triple_of_lst)
